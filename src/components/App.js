60a886eb79990b9686d2923dac8e3b19702d3800
Brianna Kincart <briannakincart@gmail.com>
2018-12-18 10:47:53 -0500

    More specific id naming in handleClick function

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 const App = props => {
-  let handleClick = id => {
-    let specificSongs = props.data.songs.filter(song => song.movieId === id)
+  let handleClick = clickedMovieId => {
+    let specificSongs = props.data.songs.filter(song => song.movieId === clickedMovieId)
     let alertMessage = `The songs for this movie are:\n`
     specificSongs.forEach(song => {
       alertMessage += `${song.title}\n`
